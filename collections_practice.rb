4909dd5c07df2b060ef4b55835881ec787efe195
Adam Clayman <adam@clayman.org>
2017-10-17 01:58:15 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -2,3 +2,17 @@ def sort_array_asc(array)
   array.sort
 end
 
+def sort_array_desc(array)
+  array.sort.reverse
+end
+
+def sort_array_char_count(array)
+  array.sort do |a, b|
+    if a.length < b.length
+      1
+    elsif a.length == b.length
+      0
+    elsif a.length > b.length
+      -1
+  end
+end
