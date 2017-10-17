4d2a971a19e6ac2f0b578ee1050450273f4f2f82
Adam Clayman <adam@clayman.org>
2017-10-17 01:59:16 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -9,11 +9,11 @@ end
 def sort_array_char_count(array)
   array.sort do |a, b|
     if a.length < b.length
-      1
+      -1
     elsif a.length == b.length
       0
     elsif a.length > b.length
-      -1
+      1
     end
   end
 end
